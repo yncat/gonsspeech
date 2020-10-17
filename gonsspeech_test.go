2e45987b6c3af86e7d0e022d13352e6668295eb8
nyanchan <personal@nyanchangames.com>
2020-10-17 16:01:21 +0900

    Add speech test case

--- gonsspeech_test.go
+++ gonsspeech_test.go
@@ -2,6 +2,7 @@ package gonsspeech
 import (
 "testing"
 "github.com/stretchr/testify/assert"
+"time"
 )
 
 func TestNsSpeech_Init(t *testing.T) {
@@ -9,5 +10,14 @@ err := NsSpeechInit()
 assert.NoError(t, err, "NsSpeechInit")
 err = NsSpeechInit()
 assert.EqualError(t, err, "Cannot initialize NsSpeechSynthesizer interface.")
+NsSpeechFree()
+}
+
+func TestNsSpeech_Speak(t *testing.T) {
+err := NsSpeechInit()
+assert.NoError(t, err, "NsSpeechInit")
+err := NsSpeechSpeak("test")
+assert.NoError(t, err, "NsSpeechSpeak")
+time.Sleep(2)
 NsSpeechFree()
 }
