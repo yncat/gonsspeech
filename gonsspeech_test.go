8460c56f8dcefddc1824aae80ddc735db86b73ae
nyanchan <personal@nyanchangames.com>
2020-10-24 22:43:30 +0900

    Add setRate test

--- gonsspeech_test.go
+++ gonsspeech_test.go
@@ -28,6 +28,35 @@ func TestNsSpeech_Speak_Error(t *testing.T) {
 	assert.EqualError(t, err, "NsSpeechSynthesizer interface has not been initialized.")
 }
 
+func TestNsSpeech_SetRate(t *testing.T) {
+	err := NsSpeechInit()
+	assert.NoError(t, err, "NsSpeechInit")
+
+	err = NsSpeechSpeak("rate 240 default")
+	assert.NoError(t, err, "NsSpeechSpeak")
+	time.Sleep(2 * time.Second)
+
+err := NsSpeechSetRate(480)
+	assert.NoError(t, err, "NsSpeechSetRate")
+	err = NsSpeechSpeak("rate 480")
+	assert.NoError(t, err, "NsSpeechSpeak")
+	time.Sleep(2 * time.Second)
+
+	NsSpeechFree()
+}
+
+func TestNsSpeech_SetRate_Error(t *testing.T) {
+	err := NsSpeechSetRate(400)
+	assert.EqualError(t, err, "NsSpeechSynthesizer interface has not been initialized.")
+	err = NsSpeechInit()
+	assert.NoError(t, err, "NsSpeechInit")
+
+	// There's no highest value limit since apple developers guide doesn't mention it. But I'm pretty sure there is.
+	err = NsSpeechSetRate(-1)
+	assert.EqualError(t, err, "rate value is out of range")
+	NsSpeechFree()
+}
+
 func TestNsSpeech_IsSpeaking(t *testing.T) {
 	err := NsSpeechInit()
 	assert.NoError(t, err, "NsSpeechInit")
