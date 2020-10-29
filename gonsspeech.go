d58ccf14cabdaa442eac83b9cc9aef550132aaf9
Keith <keith.michael.wipf@prsdmb.ca>
2020-10-29 14:34:58 -0500

    Try fix

--- gonsspeech.go
+++ gonsspeech.go
@@ -1,9 +1,54 @@
 package gonsspeech
 
 /*
-//#cgo LDFLAGS: -L./objective-c -lnsspeech
-#CGO CFLAGS: -x objective-c
-#include "${SRCDIR}/objective-c/splib.m"
+#cgo CFLAGS: -x objective-c
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+NSSpeechSynthesizer *speechSynth = NULL;
+
+int NsSpeechInit(){
+if(speechSynth) return 0;
+speechSynth=[[NSSpeechSynthesizer alloc] init];
+[speechSynth setRate:240];
+[speechSynth setVolume:0.9];
+return 1;
+}
+
+int NsSpeechFree(){
+if(!speechSynth) return 0;
+speechSynth=NULL;
+return 1;
+}
+
+int NsSpeechSpeak(char *str){
+if(!speechSynth) return 0;
+NSString *nsstr = [NSString stringWithCString: str encoding:NSUTF8StringEncoding];
+[speechSynth startSpeakingString:nsstr];
+usleep(5000);
+    return 1;
+}
+
+int NsSpeechStop(){
+if(!speechSynth) return 0;
+[speechSynth stopSpeaking];
+    return 1;
+}
+
+int NsSpeechSetRate(float rate){
+if(!speechSynth) return 0;
+if(rate<=0) return -1;
+[speechSynth setRate:rate];
+return 1;
+}
+
+int NsSpeechIsSpeaking(){
+if(!speechSynth) return 0;
+return [speechSynth isSpeaking] == YES ? 1 : 2;
+}
+
+
 */
 import "C"
 import (
